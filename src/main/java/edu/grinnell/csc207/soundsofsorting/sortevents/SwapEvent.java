0ec6a5eb8552bb5b22b2c88097473bd7673be7ce
Drilon Qerimi <drilonqerimi@Drilons-MacBook-Air.local>
2025-03-30 13:55:14 -0500

    Finished implementing swap  event

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
@@ -1,8 +1,35 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A <code>SwapEvent</code> logs a swap between two indices of the array.
  */
 public class SwapEvent<T> {
-    // TODO: implement me
+    private int id1; // first index
+    private int id2; // second index
+
+    public SwapEvent(int id1, int id2) {
+        this.id1 = id1;
+        this.id2 = id2;
+    }
+
+    public void apply(T[] arr) {
+        T tmp = arr[id1];
+        arr[id1] = arr[id2];
+        arr[id1] = tmp;
+    }
+
+    public List<Integer> getAffectedIndices() {
+        List<Integer> index = new ArrayList<>();
+        index.add(id1);
+        index.add(id2);
+        return index;
+    }
+
+    public boolean isEmphasized() {
+        return true;
+    }
+
 }
