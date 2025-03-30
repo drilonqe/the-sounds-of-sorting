819f1864d09655781f70faf12aaacc643a3fc4ef
Drilon Qerimi <drilonqerimi@Drilons-MacBook-Air.local>
2025-03-30 13:11:36 -0500

    Finished implementing copy event

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
@@ -1,8 +1,30 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A <code>CopyEvent</code> logs a copy of a value into an index of the array.
  */
 public class CopyEvent<T> {
-    // TODO: implement me!
+private int dest; 
+private T val;
+
+public CopyEvent(int dest, T val) {
+    this.dest = dest;
+    this.val = val;
+}
+ public void apply(T[] arr) {
+    arr[dest] = val; // copy value at the index of destination
+    }
+
+    public List<Integer> getAffectedIndices() {
+        List<Integer> index = new ArrayList<>();
+        index.add(dest); // only index of destination is affected
+        return index;
+    }
+
+    public boolean isEmphasized() {
+        return true; 
+    }
 }
