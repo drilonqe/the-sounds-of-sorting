c977abf6420cd1f5e8831f08a31077c8df334d69
Drilon Qerimi <drilonqerimi@Drilons-MacBook-Air.local>
2025-03-30 18:49:20 -0500

    Completed all sorting functions and testing

--- src/test/java/edu/grinnell/csc207/soundsofsorting/SortsTests.java
+++ src/test/java/edu/grinnell/csc207/soundsofsorting/SortsTests.java
@@ -41,25 +41,98 @@ public class SortsTests {
     @Test
     public void testBubbleSort() {
         testSort(Sorts::bubbleSort);
+
+        Integer[] arr1 = {1, 2, 3, 4, 5};
+        Sorts.bubbleSort(arr1);
+        assertTrue(sorted(arr1));
+
+        Integer[] arr2 = {1, 7, 3, 4, 15, 0, 9, 2};
+        Sorts.bubbleSort(arr2);
+        assertTrue(sorted(arr2));
+
+        Integer[] arr3 = {1, 2, 1, 3, 3, 6, 7, 2, 10};
+        Sorts.bubbleSort(arr3);
+        assertTrue(sorted(arr3));
     }
     
     @Test
     public void testInsertionSort() {
         testSort(Sorts::insertionSort);
+
+        Integer[] arr1 = {5, 4, 3, 2, 1};
+        Sorts.insertionSort(arr1);
+        assertTrue(sorted(arr1));
+
+        Integer[] arr2 = {1, 7, 3, 7, 5, 4, 4, 15, 0, 9, 2};
+        Sorts.insertionSort(arr2);
+        assertTrue(sorted(arr2));
+
+        Integer[] arr3 = {1};
+        Sorts.insertionSort(arr3);
+        assertTrue(sorted(arr3));
     }
     
     @Test
     public void testSelectionSort() {
         testSort(Sorts::selectionSort);
+        Integer[] arr1 = {-1, -2, -3, -4, -5};
+        Sorts.selectionSort(arr1);
+        assertTrue(sorted(arr1));
+
+        Integer[] arr2 = {1,3,5,4,2};
+        Sorts.selectionSort(arr2);
+        assertTrue(sorted(arr2));
+
+        Integer[] arr3 = {1, 1,2,2,2};
+        Sorts.selectionSort(arr3);
+        assertTrue(sorted(arr3));
     }
 
     @Test
     public void testMergeSort() {
         testSort(Sorts::mergeSort);
+        Integer[] arr1 = {7,6,5,-2,-1,-3};
+        Sorts.mergeSort(arr1);
+        assertTrue(sorted(arr1));
+
+        Integer[] arr2 = {};
+        Sorts.mergeSort(arr2);
+        assertTrue(sorted(arr2));
+
+        Integer[] arr3 = {9,8,7,6,5,1,2,3,4};
+        Sorts.mergeSort(arr3);
+        assertTrue(sorted(arr3));
     }
     
     @Test
     public void testQuickSort() {
         testSort(Sorts::quickSort);
+        Integer[] arr1 = {1, 2, 3, 4, 5};
+        Sorts.quickSort(arr1);
+        assertTrue(sorted(arr1));
+
+        Integer[] arr2 = {0,0,0,0};
+        Sorts.quickSort(arr2);
+        assertTrue(sorted(arr2));
+
+        Integer[] arr3 = {3,3,5,5,6,6,7,8,9,5,4,3,2};
+        Sorts.quickSort(arr3);
+        assertTrue(sorted(arr3));
     }
+
+    @Test
+    public void testgnomeSort() {
+        Integer[] arr1 = {1, 2, 3, 4, 5};
+        Sorts.gnomeSort(arr1);
+        assertTrue(sorted(arr1));
+
+        Integer[] arr2 = {2,5,3,4,7,1};
+        Sorts.gnomeSort(arr2);
+        assertTrue(sorted(arr2));
+
+        Integer[] arr3 = {3,3,5,5,6,6,7,8,9,5,4,3,2};
+        Sorts.gnomeSort(arr3);
+        assertTrue(sorted(arr3));
+    }
+
 }
