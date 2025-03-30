8a3063e1d6b5321af3da4eae46f7afb0d89e3cad
Drilon Qerimi <drilonqerimi@Drilons-MacBook-Air.local>
2025-03-30 12:53:14 -0500

    Finished implementing compare event

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
@@ -1,9 +1,34 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A <code>CompareEvent</code> logs a comparison a sort makes between two
  * indices in the array.
  */
 public class CompareEvent<T> {
-    // TODO: implement me!
+    private int i;
+    private int j;
+    public CompareEvent(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public void apply(T[] arr) {
+        
+    }
+
+    public List<Integer> getAffectedIndices() {
+        List<Integer> index = new ArrayList<>();
+        index.add(i);
+        index.add(j);
+        return index;
+    }
+
+    public boolean isEmphasized() {
+        return false;
+    }
+    
 }
